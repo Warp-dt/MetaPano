0915720ed3f79b910e193804710b13a9fae01b7f
Warp-dt <109367617+Warp-dt@users.noreply.github.com>
2025-10-08 23:23:40 +0200

    fix deuxieme regex

--- main.py
+++ main.py
@@ -1213,7 +1213,7 @@ Tous les stuffs que le bot va recommander sont présents dans ce compte dofusboo
 async def change_bibliotheque_canal(interaction: Interaction, lien_biblio: str, nom_biblio: str, dossier: Optional[str] = "tout"):
 
     # Check if the link sent is a valid dofusbook link
-    if not re.match(r"https?://(d-bk\.net|touch\.dofusbook\.net)/fr/(tl/\w+|membre/\d+-\w+/equipements)", lien_biblio):
+    if not re.match(r"https?://(d-bk\.net|(touch|retro|www)\.dofusbook\.net)/fr/((t|d|r)l/\w+|membre/\d+-\w+/equipements)", lien_biblio):
         print(f"Invalid dofusbook link: {lien_biblio}")
         # Return embed with an error message
         embed = Embed(
