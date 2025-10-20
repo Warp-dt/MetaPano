f96776eeb2e1c9ff4905d4b0455ffc3ca9d3b633
Warp-dt <109367617+Warp-dt@users.noreply.github.com>
2025-10-20 15:27:29 +0200

    rajout d'images et emoji

--- responses.py
+++ responses.py
@@ -29,54 +29,9 @@ ELEMENTS=['terre', 'feu', 'eau', 'air', 'dopou', 'feu+terre', 'eau+terre',
           'dopou+feu+terre', 'air+dopou+feu', 'dopou+eau+feu', 'dopou+eau+terre', 
           'multi',"vide"]
 CLASSES=['cra', 'ecaflip', 'eniripsa', 'enutrof', 'feca', 'iop', 'osamodas', 'pandawa',
-         'roublard','sacrieur', 'sadida', 'sram', 'steamer', 'xelor','zobal',"eliotrope","huppermage","ouginak","forgelance",'vide']
+         'roublard','sacrieur', 'sadida', 'sram', 'steamer', 'xelor','zobal',
+         "eliotrope","huppermage","ouginak","forgelance",'vide']
 #dans elements et classes je rajoute vide et faux pour prendre en compte les cas où on ne remplis pas l'argument de l'un des deux, ça peut être normal
-
-STUFFS=dict()
-STUFFS["terre"]={'kolo lunaire roxx': ["<https://d-bk.net/fr/t/8vYb>"],'kolo lunaire res': ["<https://d-bk.net/fr/t/ArAs>"],'kolo lunaire goule': ["<https://d-bk.net/fr/t/ANaF>"],'kolo chasseur ré fixes': ["<https://d-bk.net/fr/t/B0wS>"], 'scaramouchapeau': ["<https://d-bk.net/fr/t/9WxC>"],'plistik wulan': ["<https://d-bk.net/fr/t/BpSq>"], 'cc': [], 'no_cc': []}
-STUFFS["feu"]={'bandit lumi': ["<https://d-bk.net/fr/t/A8Zv>"], 'funespadon': ["<https://d-bk.net/fr/t/8yAu>"], 'bandit firefoux': ["<https://d-bk.net/fr/t/7yVX>"], 'no_cc': []}
-STUFFS["eau"]={'sanctuaire cryo': ["<https://d-bk.net/fr/t/B7er>"], 'sanctuaire wulan': ["<https://d-bk.net/fr/t/B9MR>"], 'cc': [], 'no_cc': []}
-STUFFS["air"]={'no pano, dina usicke': ["<https://d-bk.net/fr/t/9pdy>"], 'no pano, klime sacrif': ["<https://d-bk.net/fr/t/9RJp>"],'ava padgref': ["<https://d-bk.net/fr/t/5qJF>"],'ava allister': ["<https://d-bk.net/fr/t/2EdT>"], 'cc': [], 'no_cc': []}
-STUFFS["feu+terre"]={'12/6': [], '11/6': [], 'cc': ["<https://d-bk.net/fr/t/7ESW>"], 'no_cc': ["<https://d-bk.net/fr/t/AwSG>"]}
-STUFFS["eau+terre"]={'12/6': [],'11/6': [], 'noctu wulan': ["<https://d-bk.net/fr/t/Azb9>"], 'cc': [], 'no_cc': []}
-STUFFS["air+terre"]={'nevark virale': ["<https://d-bk.net/fr/t/Bvq3>"], '11/6': [], 'cc': [], 'no_cc': []}
-# STUFFS["dopou+terre"]={'12/6': [], '11/6': [], 'cc': [], 'no_cc': []}
-STUFFS["eau+feu"]={'12/6': [], '11/6': [], 'Mazin pnose': ["<https://d-bk.net/fr/t/931X>"], 'cc': [], 'no_cc': []}
-STUFFS["air+feu"]={'12/6': [], '11/6': [],'cc': [], 'Padgref strigide': ["<https://d-bk.net/fr/t/7OhS>"], 'no_cc': []}
-# STUFFS["dopou+feu"]={'12/6': [], '11/6': [], 'cc': [], 'no_cc': []}
-STUFFS["air+eau"]={'12/6': ["<https://d-bk.net/fr/t/8Ql4>"], '11/6': ["<https://d-bk.net/fr/t/BMXD>"], 'cc': ["<https://d-bk.net/fr/t/Br83>"], 'no_cc': []}
-STUFFS["dopou+eau"]={'12/6': [], 'full dopou': ["<https://d-bk.net/fr/t/AzPu>"], 'cc': [], 'no_cc': []}
-STUFFS["air+dopou"]={'12/6': [], '11/6': [],'ava égarés': ["<https://d-bk.net/fr/t/B150>"], 'full dopou': ["<https://d-bk.net/fr/t/BK5m>"], 'cc': [], 'no_cc': []}
-STUFFS["air+eau+terre"]={'12/6': [], '11/6': ["<https://d-bk.net/fr/t/2OhT>"], 'cc': [], 'no_cc': []}
-STUFFS["air+eau+feu"]={'12/6': [], '11/6': [], 'cc': ["<https://d-bk.net/fr/t/2OhM>"], 'no_cc': ["<https://d-bk.net/fr/t/BT4t>"]}
-STUFFS["air+feu+terre"]={'12/6': [], '11/6': [], 'cc': ["<https://d-bk.net/fr/t/4qgX>"], 'no_cc': ["<https://d-bk.net/fr/t/AP7x>"]}
-STUFFS["eau+feu+terre"]={'12/6': [], '11/6': [], 'cc': ["<https://d-bk.net/fr/t/47TD>"], 'no_cc': []}
-STUFFS["air+dopou+eau"]={'12/6': ["<https://d-bk.net/fr/t/5kTX>"], '11/6': ["<https://d-bk.net/fr/t/A9Iy>"], 'cc': [], 'no_cc': []}
-# STUFFS["air+dopou+terre"]={'12/6': [], '11/6': [], 'cc': [], 'no_cc': []}
-# STUFFS["dopou+feu+terre"]={'12/6': [], '11/6': [], 'cc': [], 'no_cc': []}
-# STUFFS["air+dopou+feu"]={'12/6': [], '11/6': [], 'cc': [], 'no_cc': []}
-STUFFS["dopou+eau+feu"]={'12/6': [], 'full dopou': ["<https://d-bk.net/fr/t/BQQd>"], 'cc': [], 'no_cc': []}
-STUFFS["dopou+eau+terre"]={'12/6': [], 'full dopou': ["<https://d-bk.net/fr/t/BQQl>"], 'cc': [], 'no_cc': []}
-STUFFS["multi"]={'12/6': [], '11/6': [], 'strigide glours': ["<https://d-bk.net/fr/t/2Ol7>"], 'virale glours wukin': ["<https://d-bk.net/fr/t/Bv6e>"]}
-STUFFS["dopou"]={'eau air dopou 11/6' : [STUFFS["air+dopou+eau"]["11/6"][0]]
-                ,'eau air dopou 12/6' : [STUFFS["air+dopou+eau"]["12/6"][0]]
-                ,'eau dopou' : [STUFFS["dopou+eau"]["full dopou"][0]]
-                ,'air dopou' : [STUFFS["air+dopou"]["full dopou"][0]]
-                ,'terre eau dopou' : [STUFFS["dopou+eau+terre"]["full dopou"][0]]
-                ,'feu eau dopou' : [STUFFS["dopou+eau+feu"]["full dopou"][0]]
-                } ### Dopou is always with another element
-STUFFS["osamodas"]={'feu' :{'12/6' :["<https://d-bk.net/fr/t/9nwN>"]}
-                    ,'eau+feu':{'cher' : ["<https://d-bk.net/fr/t/9UXA>"],'plus abordable' : ["<https://d-bk.net/fr/t/AQSn>"]}
-                    ,'air' :{'11/6' :["<https://d-bk.net/fr/t/9Y06>"]}
-                    ,'eau' :{'12/6' :["<https://d-bk.net/fr/t/BCMB>"]}                    
-                    }
-STUFFS["sadida"]={'feu' :{'bandit luminescente' :["<https://d-bk.net/fr/t/9nwN>"]}
-                    ,'eau+feu':{'mazin pnose' : ["<https://d-bk.net/fr/t/9UXA>"]}
-                    ,'terre' :{'scaramouchapeau lunaire' :["<https://d-bk.net/fr/t/BQpf>"]}
-                    # ,'eau+terre' :{'11/6' :["<https://d-bk.net/fr/t/BoWN>"]}                
-                    }
-STUFFS["iop"]={'dopou' :{'12/6' :["<https://d-bk.net/fr/t/5kTX>"]}              
-                    }
 
 IMAGES_LINK=dict()
 IMAGES_LINK["twitch"]="https://drive.google.com/uc?id="+"1B7dARplU3Y-0zS77S_JTSqh1gu-tvyIa"#https://drive.google.com/file/d/1B7dARplU3Y-0zS77S_JTSqh1gu-tvyIa/view?usp=sharing
@@ -109,6 +64,7 @@ IMAGES_LINK["dopou+eau+terre"]= "https://drive.google.com/uc?id="+"1pDqCVYu4HXpf
 IMAGES_LINK["air+eau+feu+terre"]="https://drive.google.com/uc?id="+"1sFla0c4Ze-AkuTM_ubTYjLzviTyu-c2f"#https://drive.google.com/file/d/1sFla0c4Ze-AkuTM_ubTYjLzviTyu-c2f/view?usp=sharing
 IMAGES_LINK["multi"]=           "https://drive.google.com/uc?id="+"1sFla0c4Ze-AkuTM_ubTYjLzviTyu-c2f"#https://drive.google.com/file/d/1sFla0c4Ze-AkuTM_ubTYjLzviTyu-c2f/view?usp=sharing
 IMAGES_LINK["vide"]=IMAGES_LINK["multi"]
+
 IMAGES_LINK["livre"]=           "https://drive.google.com/uc?id="+"1dgPyJFURlrr79gsMyoWTPFo8NntDkXob"#https://drive.google.com/file/d/1dgPyJFURlrr79gsMyoWTPFo8NntDkXob/view?usp=sharing
 IMAGES_LINK["harry"]=           "https://drive.google.com/uc?id="+"1vm8hxVat2uv0zMs7lsa2dF31Fkb_AkFw"#https://drive.google.com/file/d/1vm8hxVat2uv0zMs7lsa2dF31Fkb_AkFw/view?usp=sharing
 IMAGES_LINK["youtube"]=           "https://drive.google.com/uc?id="+"17LoeB0RmVWyD4EL3kX8toXzIISW-jlu6"#https://drive.google.com/file/d/17LoeB0RmVWyD4EL3kX8toXzIISW-jlu6/view?usp=sharing
@@ -129,58 +85,10 @@ IMAGES_LINK["sram"]=        "https://drive.google.com/uc?id="+"1TLoJ1Dg-Y31aIrPD
 IMAGES_LINK["steamer"]=     "https://drive.google.com/uc?id="+"1IV_-lIEsKEvfmOVOpdTelHAAYBCgWe6h"#https://drive.google.com/file/d/1IV_-lIEsKEvfmOVOpdTelHAAYBCgWe6h/view?usp=sharing
 IMAGES_LINK["xelor"]=       "https://drive.google.com/uc?id="+"1p_DCP4it8tL7EEradWE7B0e3bCjPIl21"#https://drive.google.com/file/d/1p_DCP4it8tL7EEradWE7B0e3bCjPIl21/view?usp=sharing
 IMAGES_LINK["zobal"]=       "https://drive.google.com/uc?id="+"1Feh1asb3iKvVvB5ABcrAiyDz35VTkiUO"#https://drive.google.com/file/d/1Feh1asb3iKvVvB5ABcrAiyDz35VTkiUO/view?usp=sharing
-
-#####################
-# MAIN RESPONSE
-#####################
-
-# def get_response(user_input: str, user_name : str, channel : str):
-#     lowered: str = user_input.lower()
-
-#     if lowered == '':
-#         return 'Ok tu arrive à envoyer des messages vides?? Sorcier!'
-#     elif lowered =='salut warpbot' :
-#         return f'Salut {user_name}!'    
-#     elif lowered =='!warpbot lance un dé' or lowered =='!wb lance un dé':
-#         return f'Tu as eu: {randint(1, 6)}'
-#     elif command :=re.match(r"(?P<botname>!\w+)\s?(?P<function>\w*)\s?(?P<arg1>[+\w]*)\s?(?P<arg2>[a-z0-9\/\.\-:]*)", lowered):
-#         print(command.group('botname'),command.group('function'),command.group('arg1'),command.group('arg2'))
-#         if command.group('botname')!= '!warpbot' and command.group('botname')!= '!wb':
-#             return -1
-        
-#         if command.group('function')=="stuff": #stuff
-#             element = command.group('arg1')
-#             classe = command.group('arg2')
-#             # print(element,classe)
-#             return stuff_response(element=element,classe=classe)
-        
-#         # elif command.group('function')=="calcul": #calcul
-#         #     return calcul_response(command)
-        
-#         elif command.group('function')=="twitch": #twitch
-#             resp= f"""
-# Je stream la majorité des tournois pvp sur dofus touch, sauf quand je participe bien sur !
-# Au programme :
-# - 27/28/29 septembre : stream tournois du serveur Oshimo
-# - 4/5/6 octobre : je participe au tournois sur herdegrize
-# """
-#             return resp
-        
-#         elif command.group('function')=="help": #help
-#             return help_response(command)
-        
-#         else: #mauvaise fonction
-#             resp= f"""
-# Argument `{command.group('function')} ` inconnu, les fonctions utilisables sont:
-# - help   : pour recevoir de l'aide sur l'utilisation du bot 
-# - stuff  : pour recevoir des recommandations de stuff
-# - twitch : pour avoir des infos sur les prochains stream de warp
-# Tu peux utiliser `!WarpBot help xxx` pour avoir des informations sur comment formuler les requetes avec chacune des 2 fonctions stuff/twitch.
-# """
-# # - calcul : pour avoir des estimations de dommage d'invocations ou dopou
-#             return resp
-#     return -1
-
+IMAGES_LINK["eliotrope"]=   "https://drive.google.com/uc?id="+"1mTO5EfMJfG2R6jybfniWUSFrXRW2sp8o"#https://drive.google.com/file/d/1mTO5EfMJfG2R6jybfniWUSFrXRW2sp8o/view?usp=sharing
+IMAGES_LINK["forgelance"]=  "https://drive.google.com/uc?id="+"1XbblEcxnM9HCDXET_Thmm2wTbRbAf9Es"#https://drive.google.com/file/d/1XbblEcxnM9HCDXET_Thmm2wTbRbAf9Es/view?usp=sharing
+IMAGES_LINK["huppermage"]=  "https://drive.google.com/uc?id="+"1oZQPKkWHvGj6IJ4iQ9lQVLFT2vvTxB1H"#https://drive.google.com/file/d/1oZQPKkWHvGj6IJ4iQ9lQVLFT2vvTxB1H/view?usp=sharing
+IMAGES_LINK["ouginak"]=     "https://drive.google.com/uc?id="+"1OKuuV-e6DCpwS3JSnIXhjV_rkF7ADUSz"#https://drive.google.com/file/d/1OKuuV-e6DCpwS3JSnIXhjV_rkF7ADUSz/view?usp=sharing
 
 
 #####################
@@ -277,117 +185,6 @@ Pour plus d'informations sur l'utilisation de ces commandes, `/help <commande>` 
 
 # Pour plus d'informations sur l'utilisation de ces commandes, tu peux taper `/help <commande>` ou aller voir la documentation sur [Dofus Touls](https://discord.gg/TcrNrRE5QV).
 
-def stuff_response(element,classe,plateforme="discord"):
-
-    elt=lecture_elt(element)
-    error=4 #0=ok, 1=element inconnu, 2= classe inconnue, 3=element&classe inconnue,4=erreurs autres
-    if plateforme=="discord":
-        prefixe='/'
-    elif plateforme=="twitch":
-        prefixe="!"
-    else :
-        prefixe='/'
-        
-
-    # vérification que les arguments soient corrects
-    if not classe in CLASSES and not elt in ELEMENTS: #classe+element non reconnus
-        resp=f"""
-Je ne reconnais pas les arguments **{elt}** et **{classe}** fournis.
-Pour recevoir de l'aide sur l'utilisation de la fonction stuff, taper `{prefixe}wbhelp stuff`.
-Elements valides: terre/feu/eau/air/dopou/multi ou toute combinaison d'éléments différents (excepté multi) séparés d'un '+', d'un '/' ou d'un espace.
-Pour les classes il faut écrire le nom en entier.
-Exemple de requete valide : `{prefixe}stuff eau+feu osamodas`.
-"""     
-        error=3
-        return resp,error
-    elif not elt in ELEMENTS: #élément non reconnu
-        resp=f"""
-Je ne reconnais pas l'élément **{elt}** désolé, pour recevoir de l'aide sur l'utilisation de la fonction stuff, taper `{prefixe}wbhelp stuff`.
-Elements valides: terre/feu/eau/air/dopou/multi ou toute combinaison d'éléments différents (excepté multi) séparés d'un '+'.
-Exemple de requete valide : `{prefixe}stuff air+eau`.
-"""     
-        error=1
-        return resp,error
-    elif not classe in CLASSES: #classe non reconnue
-        resp=f"""
-Je ne reconnais pas la classe **{classe}** désolé, pour recevoir de l'aide sur l'utilisation de la fonction stuff, taper `{prefixe}wbhelp stuff`.
-Il faut écrire le nom de classe en entier.
-Exemple de requete valide : `{prefixe}stuff eau+feu osamodas`.
-"""     
-        error=2
-        return resp,error
-    
-    if classe=='vide': # pas de classe spécifiée
-        if elt=="dopou":
-            error=0
-            resp= f"""
-Les dopou ne se jouent pas spécialement tous seuls, meme si ils sont prédominants il y a toujours un élément avec, mes recommandations sont donc les suivantes : 
-- eau air dopou : 11/6 {STUFFS["air+dopou+eau"]["11/6"][0]} ou 12/6 {STUFFS["air+dopou+eau"]["12/6"][0]}
-- eau dopou : {STUFFS["dopou+eau"]["full dopou"][0]}
-- air dopou : {STUFFS["air+dopou"]["full dopou"][0]}
-- terre eau dopou : {STUFFS["dopou+eau+terre"]["full dopou"][0]} (fonctionne pour terre dopou)
-- feu eau dopou : {STUFFS["dopou+eau+feu"]["full dopou"][0]} (fonctionne pour feu dopou)"""
-            return resp,error        
-        else:
-            if elt in STUFFS.keys():
-                error=0
-                resp= f"""
-Pour un stuff {elt.replace("+","/")} je recommande :\n"""
-                for mode in STUFFS[elt].keys():
-                    if len(STUFFS[elt][mode])>0:
-                        resp+=f"- {mode} : {STUFFS[elt][mode][0]}\n"
-                # resp+="N'hésite pas à tag Warp pour plus de détails sur ces stuffs."
-                return resp,error
-
-            else: #élément non présent dans la biblio
-                error=0
-                resp=f"""
-Je n'ai pas de stuff dans ma bibliothèque qui corresponde au combo {elt.replace("+","/")}, tu peux tag Warp pour savoir pourquoi et peut-être qu'il aura quelque chose à te proposer."""
-                return resp,error
-                
-    else: #avec une classe précisée
-        error=0
-        resp='pas trouvé'
-        if elt=="vide": #sans élément précisé
-            if classe in STUFFS.keys(): #si on a au moins un stuff spécifique à la classe
-                resp=f"""
-Pour l'élément {elt.replace("+","/")} de la classe {classe} je te recommande :\n"""
-                for c_elt in STUFFS[classe].keys():
-                    for mode in STUFFS[classe][c_elt].keys():
-                        if len(STUFFS[classe][c_elt][mode])>0:
-                            resp+=f"- {c_elt.replace('+','/')} | {mode} : {STUFFS[classe][c_elt][mode][0]}\n"
-                # resp+="N'hésite pas à tag Warp pour plus de détails sur ces stuffs."
-                    
-            if resp=='pas trouvé':
-                resp=f"""
-    Je n'ai pas de stuff {elt.replace("+","/")} spécifiques pour la classe {classe}, tu trouveras probablement ton bonheur dans les stuffs classiques de l'élément:\n"""
-                for mode in STUFFS[elt].keys():
-                    if len(STUFFS[elt][mode])>0:
-                        resp+=f"- {mode} : {STUFFS[elt][mode][0]}\n"
-                # resp+="N'hésite pas à tag Warp pour plus de détails sur ces stuffs."
-            return resp,error
-        else: #avec un élément
-            if classe in STUFFS.keys():
-                if elt in STUFFS[classe].keys():
-                    resp=f"""
-    Pour l'élément {elt.replace("+","/")} de la classe {classe} je te recommande :\n"""
-                    for mode in STUFFS[classe][elt].keys():
-                        if len(STUFFS[classe][elt][mode])>0:
-                            resp+=f"- {mode} : {STUFFS[classe][elt][mode][0]}\n"
-                    # resp+="N'hésite pas à tag Warp pour plus de détails sur ces stuffs."
-                    
-            if resp=='pas trouvé':
-                resp=f"""
-    Je n'ai pas de stuff {elt.replace("+","/")} spécifiques pour la classe {classe}, tu trouveras probablement ton bonheur dans les stuffs classiques de l'élément:\n"""
-                for mode in STUFFS[elt].keys():
-                    if len(STUFFS[elt][mode])>0:
-                        resp+=f"- {mode} : {STUFFS[elt][mode][0]}\n"
-                # resp+="N'hésite pas à tag Warp pour plus de détails sur ces stuffs."
-            return resp,error
-    error=4
-    resp="Vraisemblablement il y a une erreur dans le code : tu ne devrais pas arriver ici, tag Warp pour qu'il répare le bug stp <3"
-    return resp,error
-
 def color_mix(elements):
     
     colors_list=[COLORS[k]for k in COLORS.keys() if k in elements]
@@ -409,30 +206,3 @@ def image_response(element):
         image=IMAGES_LINK['error']
     return image
     
-def calcul_response(command,plateforme="discord"):
-
-    if plateforme=="discord":
-        prefixe='/'
-    elif plateforme=="twitch":
-        prefixe="!"
-    else :
-        prefixe='/'
-    if command.group('arg1')=="dopou": #/calcul dopou
-
-        resp= f"""
-Pour recevoir des recommandations de stuff il faut utiliser la fonction `{prefixe}stuff élément classe`
-- **élément** : terre/feu/eau/air/dopou/multi ou toute combinaison de ces éléments (excepté multi) séparés d'un '+', ex: élément+élément+... avec 3 éléments maximum
-Exemple de requete valide : `/stuff air+eau`
-- **classe** : une des classes du jeu écrite avec le nom complet, pour recevoir des stuff spécifiques à la classe donnée s'il y en a dans la bibliothèque.
-Ce qui nous fait une requete de la forme : `{prefixe}stuff eau+feu osamodas`
-"""
-    
-    elif command.group('arg1')=='': #/calcul 
-        resp= f"""
-
-"""
-    else: #/calcul dqzdzqd , arg1 erroné
-        resp= f"""
-
-"""
-    return resp
