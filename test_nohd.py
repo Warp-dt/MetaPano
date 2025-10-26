34de19ed605f8215977832b889091447710cd38c
Warp-dt <109367617+Warp-dt@users.noreply.github.com>
2025-10-26 22:08:16 +0100

    dbug

--- test_nohd.py
+++ test_nohd.py
@@ -33,8 +33,8 @@ url="https://www.dofusbook.net/api/stuffs/dofus/public/9797636"
 
 
 if __name__ == "__main__":
-    driver.get("https://ifconfig.me")
-    print(driver.page_source)
+    # driver.get("https://ifconfig.me")
+    # print(driver.page_source)
     driver.get(url)
     try:
         pre_text = driver.find_element("tag name", "pre").text
